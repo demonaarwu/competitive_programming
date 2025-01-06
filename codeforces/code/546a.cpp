a3635acc0095ef6de6234ae40bff065a9b29819e
Aaron Wu <demonaarwu@tuta.io>
2025-01-06 09:37:28 +0800

    Codeforces 546a

---
+++ codeforces/code/546a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int k, n, w;
+    cin >> k >> n >> w;
+
+    int cost = w * (k + k*w) / 2;
+
+    if (cost <= n)
+    {
+	cout << 0 << endl;
+    }
+    else
+    {
+	cout << cost-n << endl;
+    }
+}
