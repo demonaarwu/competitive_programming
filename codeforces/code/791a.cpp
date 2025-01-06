08d58531d6c77425c5b21f008b4e0c913029b31d
Aaron Wu <demonaarwu@tuta.io>
2025-01-06 09:21:50 +0800

    Codeforces 791a

---
+++ codeforces/code/791a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    int year = 0;
+
+    while (a <= b)
+    {
+	year++;
+	a *= 3;
+	b *= 2;
+    }
+
+    cout << year << endl;
+}
