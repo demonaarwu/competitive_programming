d20c22922f7ead7d583d4267df1f3a75fa2c596b
Aaron Wu <demonaarwu@tuta.io>
2025-01-06 09:44:12 +0800

    Codeforces 734a

---
+++ codeforces/code/734a.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    map<char, int> games;
+
+    string s;
+    cin >> s;
+    for (int i = 0; i < n; i++)
+    {
+	games[s[i]] += 1;
+    }
+
+    if (games['A'] > games['D'])
+    {
+	cout << "Anton\n";
+    }
+    else if (games['D'] > games['A'])
+    {
+	cout << "Danik\n";
+    }
+    else
+    {
+	cout << "Friendship\n";
+    }
+}
