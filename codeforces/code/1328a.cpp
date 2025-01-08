e6da2a423f551b673961020984323f698d15bc9b
Aaron Wu <demonaarwu@tuta.io>
2025-01-09 07:26:31 +0800

    Codeforces 1328a

---
+++ codeforces/code/1328a.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+
+signed main()
+{
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    int a, b;
+    while (t--)
+    {
+        cin >> a >> b;
+
+        if (a >= b)
+        {
+            cout << (b-(a%b))%b;
+        }
+        else
+        {
+            cout << b-a;
+        }
+        cout << endl;
+    }
+}
+
