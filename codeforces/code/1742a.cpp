23d380e07c0b0aab690ecc18679282ec7d1f63f0
Aaron Wu <demonaarwu@tuta.io>
2025-01-12 14:40:26 +0800

    Codeforces 1742a

---
+++ codeforces/code/1742a.cpp
@@ -0,0 +1,35 @@
+#pragma GCC optimize ("O2")
+#include<bits/stdc++.h>
+/*-----define-----*/
+#define kidd ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define MS0(X) memset((X), 0, sizeof((X)))
+#define int long long
+/*-----typedef-----*/
+using namespace std;
+typedef long long LL;
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<PII> VPII;
+
+signed main()
+{
+    int t;
+    cin >> t;
+
+    int arr[3];
+    while (t--)
+    {
+        cin >> arr[0] >> arr[1] >> arr[2];
+
+        sort(arr, arr+3);
+        if (arr[0] + arr[1] == arr[2])
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+	return 0;
+}
