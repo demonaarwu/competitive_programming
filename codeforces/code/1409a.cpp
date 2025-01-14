102623778944aacca5133c5c8730fca339cf9caf
Aaron Wu <demonaarwu@tuta.io>
2025-01-14 10:21:24 +0800

    Codeforces 1409a

---
+++ codeforces/code/1409a.cpp
@@ -0,0 +1,35 @@
+#pragma GCC optimize ("O2")
+#include<bits/stdc++.h>
+/*-----define-----*/
+#define kidd ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define MS0(X) memset((X), 0, sizeof((X)))
+#define int long long
+/*-----typedef-----*/
+using namespace std;
+typedef long long LL;
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<PII> VPII;
+
+signed main()
+{
+	kidd;
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        if (a == b)
+        {
+            cout << 0 << endl;
+        }
+        else
+        {
+            cout << (abs(a-b)/10) + ((abs(a-b) % 10) != 0) << endl;
+        }
+    }
+	return 0;
+}
