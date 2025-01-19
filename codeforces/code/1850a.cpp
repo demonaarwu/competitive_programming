fc107067a686a7af1e5e052acd736a166739509e
Aaron Wu <demonaarwu@tuta.io>
2025-01-19 08:07:50 +0800

    Codeforces 1850a

---
+++ codeforces/code/1850a.cpp
@@ -0,0 +1,36 @@
+#pragma GCC optimize ("O2")
+#include<bits/stdc++.h>
+/*-----define-----*/
+#define kidd ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define MS0(X) memset((X), 0, sizeof((X)))
+#define int long long
+/*-----typedef-----*/
+using namespace std;
+typedef long long LL;
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<PII> VPII;
+
+signed main()
+{
+	//kidd;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if ((a+b+c) - min(a, min(b, c)) >= 10)
+        {
+            cout << "yes\n";
+        }
+        else
+        {
+            cout << "no\n";
+        }
+    }
+	return 0;
+}
