036ccf045b796049403e5d995f8bbf812f1b8f88
Aaron Wu <demonaarwu@tuta.io>
2025-01-22 15:18:16 +0800

    Codeforces 2009a

---
+++ codeforces/code/2009a.cpp
@@ -0,0 +1,29 @@
+#pragma GCC optimize ("O2")
+#include<bits/stdc++.h>
+/*-----define-----*/
+#define kidd ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define MS0(X) memset((X), 0, sizeof((X)))
+#define int long long
+/*-----typedef-----*/
+using namespace std;
+typedef long long LL;
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<PII> VPII;
+
+signed main()
+{
+	//kidd;
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+
+        cout << b - a << endl;
+        
+    }
+	return 0;
+}
