3d7b48219d5555dfa5beab53f980a74349118e30
Aaron Wu <demonaarwu@tuta.io>
2025-01-27 09:16:02 +0800

    Codeforces 1283a

---
+++ codeforces/code/1283a.cpp
@@ -0,0 +1,30 @@
+#pragma GCC optimize ("O2")
+#include<bits/stdc++.h>
+/*-----define-----*/
+#define kidd ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define MS0(X) memset((X), 0, sizeof((X)))
+#define int long long
+/*-----typedef-----*/
+using namespace std;
+typedef long long LL;
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<PII> VPII;
+
+signed main()
+{
+	//kidd;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int hours;
+        int minutes;
+        cin >> hours >> minutes;
+
+        cout << 1440-hours*60-minutes << endl;
+    }
+	return 0;
+}
